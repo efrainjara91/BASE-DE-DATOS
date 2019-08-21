62ee73adba030c317e6bbd86e6297c6798af2824
efrainjara91 <54295790+efrainjara91@users.noreply.github.com>
2019-08-20 23:22:16 -0400

    Create Ejercicio03.sql

---
+++ Ejercicio03.sql
@@ -0,0 +1,15 @@
+drop table if exists agenda;
+
+create table agenda (
+    nombre varchar(20),
+    domicilio varchar(30),
+    telefono varchar(11)
+);
+
+show tables;
+
+describe agenda;
+
+drop table, if exists;
+
+drop table agenda;
