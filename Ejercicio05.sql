9f61834aac4dd0311505494ea43f17986f750a67
efrainjara91 <54295790+efrainjara91@users.noreply.github.com>
2019-08-20 23:39:01 -0400

    Update Ejercicio05.sql

--- Ejercicio05.sql
+++ Ejercicio05.sql
@@ -1,8 +1,8 @@
 drop table if exists peliculas;
 create table peliculas(nombre varchar(20), actor varchar(20), duracion integer,cantidad integer);
 describe peliculas;
-insert into peliculas (nombre, actor, duracion ,cantidad) values ('John Wick 3','Keanu Reeves',130,3);
-insert into peliculas (nombre, actor, duracion ,cantidad) values  ('Spider-Man: lejos de casa','Tom Holland',129,2);
-insert into peliculas (nombre, actor, duracion, cantidad) values ('La razón de estar contigo: un nuevo viaje','Dennis Quaid',98,3);
-insert into peliculas (nombre, actor, duracion, cantidad) values ('Rápidos y furiosos: Hobbs & Shaw','Jason Statham',136,2);
+insert into peliculas (nombre, actor, duracion ,cantidad) values ('John Wick 3','Keanu Reeves',130,55);
+insert into peliculas (nombre, actor, duracion ,cantidad) values  ('Spider-Man: lejos de casa','Tom Holland',129,32);
+insert into peliculas (nombre, actor, duracion, cantidad) values ('La razón de estar contigo: un nuevo viaje','Dennis Quaid',98,15);
+insert into peliculas (nombre, actor, duracion, cantidad) values ('Rápidos y furiosos: Hobbs & Shaw','Jason Statham',136,20);
 select * from peliculas;
